8ab9764b6b5df499e0e88cd1e3a2c5ffcecfac9d
Amit Kumar <amit.pisces.88@gmail.com>
2021-06-11 16:38:37 +0530

    updated packages

--- cook-book/src/app/shopping/shopping-edit/shopping-edit.component.ts
+++ cook-book/src/app/shopping/shopping-edit/shopping-edit.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.sass']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  ingredients =[];
+  
   constructor() { }
 
   ngOnInit(): void {
